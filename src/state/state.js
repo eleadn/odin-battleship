78b15bd0d76d97ba55f78ff21123655190ebb4dc
eleadn <eleano.dn@gmail.com>
2025-08-07 13:40:31 +0200

    add gamestate

--- src/state/state.js
+++ src/state/state.js
@@ -1,6 +1,12 @@
 import Gameboard from "../logic/gameboard";
 
+export const GAMESTATE = {
+	NONE: 0,
+	INIT: 1,
+};
+
 export const state = {
+	gameState: GAMESTATE.NONE,
 	players: {
 		player1: {
 			type: "human",
